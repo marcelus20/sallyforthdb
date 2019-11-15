008d31142f758243ca64432819a7241ad11c42e9
marcelus <marcelus20felipe@gmail.com>
2019-11-15 03:39:54 +0000

    added indexing to the recordable entity in the schema. Also added one more table weapon 1N1 to the physcality table 'some physicality/equipment will be of an attack type'. Creater more procedures for weapons, consumables, items and other helper procedures.

--- sallyforth-schema.sql
+++ sallyforth-schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS sallyforth;
-CREATE DATABASE sallyforth;
+DROP SCHEMA IF EXISTS sallyforth;
+CREATE SCHEMA sallyforth;
 USE sallyforth;
 
 CREATE TABLE recordable(
@@ -93,10 +93,13 @@ CREATE TABLE healability(
 ##TABLE OF EQUIPMENTS
 CREATE TABLE physicality(
 	item_id int(11) not null,
-    attack int(7),
     defense int(5)
 );
-
+##table of equipments that are just weapons
+CREATE TABLE weapons(
+	physicality_id int(11) not null,
+    attack int(7)
+);
 
 
 ###junction tables
@@ -172,6 +175,10 @@ ALTER TABLE physicality
 ADD CONSTRAINT phisicalityfk
 foreign key (item_id) references item(item_id);
 
+ALTER TABLE weapons
+ADD CONSTRAINT weaponfk
+foreign key(physicality_id) references physicality(item_id);
+
 ALTER TABLE spells_learned
 ADD CONSTRAINT spells_learned_spell_idfk
 foreign key (spell_id) references item(item_id),
@@ -241,6 +248,10 @@ ALTER TABLE physicality
 ADD CONSTRAINT phisicalitypk
 primary key (item_id);
 
+ALTER TABLE weapons
+ADD CONSTRAINT weaponpk
+primary key (physicality_id);
+
 ALTER TABLE spells_learned
 ADD CONSTRAINT spells_learnedpk
 primary key (spell_id, vulnerable_id);
@@ -250,3 +261,140 @@ ALTER TABLE associations
 ADD CONSTRAINT associationpk
 primary key (association_id, item_id, vulnerable_id),
 MODIFY association_id int (11) auto_increment not null;
+
+
+
+##INDEX CREATING
+CREATE INDEX records_name ON recordable (name);
+
+
+
+
+##CREATION OF A FUNCTION THAT MAPS AN ITEM NAME IT ITS ID
+DROP FUNCTION IF EXISTS get_id;
+DELIMITER //
+CREATE FUNCTION get_id(name_ TEXT) RETURNS int READS sql data
+BEGIN
+	RETURN (SELECT id FROM recordable WHERE name = name_);
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS insert_taxonomy;
+##CREATION OF A PROCEDURE THAT GENERATES A TAXONOMY AND RETURNS THE ID
+DELIMITER //
+CREATE PROCEDURE insert_taxonomy(IN name_ text, IN type text, OUT id int)
+BEGIN
+	DECLARE id_ INT(11) DEFAULT(0);
+	INSERT INTO recordable (name) VALUE (name_);
+    set id_ = get_id(name_);
+    INSERT INTO taxonomy VALUE (id_, type);
+    SET id = id_;
+END //
+DELIMITER ;
+
+
+##CREATION OF A PROCEDURE THAT ADDS A NEW PLAYER TO THE SYSTEM WHEN RECENTLY CREATED
+DROP PROCEDURE IF EXISTS insert_player;
+DELIMITER //
+CREATE PROCEDURE insert_player(IN name_ text, IN vocation text)
+begin
+	CALL insert_taxonomy(name_, 'vulnerable', @id);
+    INSERT INTO vulnerable VALUE (@id, 'player');
+    INSERT INTO skills (vulnerable_id) VALUE (@id);
+    INSERT INTO healthy (vulnerable_id) VALUE (@id);
+    INSERT INTO players VALUE (@id, vocation);
+end //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS insert_item;
+DELIMITER //
+CREATE PROCEDURE insert_item(IN name_ text , IN weight INT, IN item_type TEXT, OUT id int)
+BEGIN
+	CALL insert_taxonomy(name_, 'item', @id);
+    INSERT INTO item VALUE (@id, weight, item_type);
+    SET id = @id;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS insert_consumable;
+DELIMITER //
+CREATE PROCEDURE insert_consumable(IN name_ text, IN weight INT, IN manapoints_to_heal TEXT, IN hitpoints_to_heal TEXT)
+BEGIN
+	CALL insert_item(name_, weight, 'consumable', @id);
+    INSERT INTO healability VALUE (@id, manapoints_to_heal, hitpoints_to_heal);
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS insert_equipment;
+DELIMITER //
+CREATE PROCEDURE insert_equipment(IN name_ text, IN weight INT, IN defense TEXT, OUT id INT)
+BEGIN
+	CALL insert_item(name_, weight, 'equipment', @id);
+    INSERT INTO physicality VALUE (@id, defense);
+    SET id = @id;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS insert_equipment_weapon;
+DELIMITER //
+CREATE PROCEDURE insert_equipment_weapon(IN name_ text, IN weight INT, IN attack TEXT, IN defense TEXT)
+BEGIN
+    CALL insert_equipment(name_, weight, defense, @id);
+    
+    INSERT INTO weapons VALUE (@id, attack);
+END //
+DELIMITER ;
+
+DROP VIEW IF EXISTS see_players;
+CREATE VIEW see_players AS 
+SELECT r.id 'PLAYER ID',
+ r.name 'PLAYER NAME',
+ t.taxonomy_type 'GROUP',
+ v.vulnerable_type 'SUB GROUP',
+ s.level 'LEVEL' ,
+ s.hitpoint 'HP',
+ s.manapoint 'MP',
+ s.capacity 'CAPACITY',
+ s.magiclevel 'MAGIC LEVEL' ,
+ s.swordfighting 'SWORD FIGHTING',
+ s.axefighting 'AXE FIGHTING' ,
+ s.clubfighting 'CLUB FIGHTING',
+ s.fishing 'FISHING',
+ h.current_hitpoint 'CURRENT HP' ,
+ h.current_manapoint 'CURRENT MP',
+ h.stamina 'STAMINA' 
+ FROM recordable r 
+	JOIN taxonomy t ON t.taxonomy_id = r.id 
+    JOIN vulnerable v ON t.taxonomy_id = v.vulnerable_id
+    JOIN skills s ON s.vulnerable_id = v.vulnerable_id
+    JOIN healthy h ON h.vulnerable_id = v.vulnerable_id;
+
+
+
+
+DROP VIEW IF EXISTS see_equipment;
+CREATE VIEW see_equipment AS
+SELECT 
+	r.id 'ID',
+    r.name 'NAME',
+    #t.taxonomy_type 'GROUP',
+    #i.item_type 'SUB GROUP',
+    i.weight 'WEIGHT',
+    p.defense 'DEFENSE' 
+    FROM recordable r 
+		JOIN taxonomy t ON r.id = t.taxonomy_id
+        JOIN item i ON i.item_id = r.id
+        JOIN physicality p ON p.item_id = r.id;
+
+DROP VIEW IF EXISTS see_weapons;
+CREATE VIEW see_weapons AS
+SELECT 
+	s.id 'ID',
+    s.name 'NAME',
+    s.weight 'WEIGHT',
+    s.defense 'DEFENSE',
+    w.attack 'ATTACK' 
+    FROM see_equipment s JOIN weapons w ON w.physicality_id = s.ID;
--- sallyforth.sql
+++ sallyforth.sql
@@ -1,50 +1,40 @@
+#####fake data generation
 
 
-DELIMITER //
-CREATE PROCEDURE insert_player(IN id INT, IN name_ text, IN vocation text)
-begin
-	INSERT INTO recordable (name) VALUE (name_);
-    INSERT INTO taxonomy VALUE (id, 'vulnerable');
-    INSERT INTO vulnerable VALUE (id, 'player');
-    INSERT INTO skills (vulnerable_id) VALUE (id);
-    INSERT INTO healthy (vulnerable_id) VALUE (id);
-    INSERT INTO players VALUE (id, vocation);
-end //
-DELIMITER ;
+###INSERTING PLAYERS
+CALL insert_player('King Arthur', 'sorcerer');
+CALL insert_player('Kendran Eliorath', 'knight');
+CALL insert_player('Judith Warrs', 'sorcerer');
+CALL insert_player('Dimmus Borgirs', 'sorcerer');
+CALL insert_player('Bendran Helliot', 'paladin');
+CALL insert_player('Murdoc Mantova', 'knight');
+CALL insert_player('Pitter Tarazz', 'paladin');
+CALL insert_player('Julius Fandoble', 'druid');
+CALL insert_player('Almighty Fernandus', 'druid');
 
-CREATE VIEW see_players AS 
-SELECT r.id 'PLAYER ID',
- r.name 'PLAYER NAME',
- t.taxonomy_type 'GROUP',
- v.vulnerable_type 'SUB GROUP',
- s.level 'LEVEL' ,
- s.hitpoint 'HP',
- s.manapoint 'MP',
- s.capacity 'CAPACITY',
- s.magiclevel 'MAGIC LEVEL' ,
- s.swordfighting 'SWORD FIGHTING',
- s.axefighting 'AXE FIGHTING' ,
- s.clubfighting 'CLUB FIGHTING',
- s.fishing 'FISHING',
- h.current_hitpoint 'CURRENT HP' ,
- h.current_manapoint 'CURRENT MP',
- h.stamina 'STAMINA' 
- FROM recordable r 
-	JOIN taxonomy t ON t.taxonomy_id = r.id 
-    JOIN vulnerable v ON t.taxonomy_id = v.vulnerable_id
-    JOIN skills s ON s.vulnerable_id = v.vulnerable_id
-    JOIN healthy h ON h.vulnerable_id = v.vulnerable_id;
 
-##@TODO = MAKE TRIGGERS TO AUTO GENERATE THE IDS.
-CALL insert_player(1,'King Arthur', 'sorcerer');
-CALL insert_player(2,'Kendran Eliorath', 'knight');
-CALL insert_player(3,'Judith Warrs', 'sorcerer');
-CALL insert_player(4,'Dimmus Borgirs', 'sorcerer');
-CALL insert_player(5,'Bendran Helliot', 'paladin');
-CALL insert_player(6,'Murdoc Mantova', 'knight');
-CALL insert_player(7,'Pitter Tarazz', 'paladin');
-CALL insert_player(8,'Julius Fandoble', 'druid');
-CALL insert_player(9,'Almighty Fernandus', 'druid');
+###inserting equipments
+CALL insert_equipment('Royal Helment','56', '29', @id);
+CALL insert_equipment('Elven Legs','30', '12', @id);
+CALL insert_equipment('Medusa Shield','12', '33', @id);
+CALL insert_equipment('Crown Armor','140', '70', @id);
+CALL insert_equipment('Demon Shield','70', '50', @id);
+
+
+##inserting weapons
+CALL insert_equipment_weapon('Fire Sword','56', '35', '23');
+CALL insert_equipment_weapon('Sword','78', '16', '10');
+CALL insert_equipment_weapon('Knight Axe','48', '36', '15');
+CALL insert_equipment_weapon('Giant Sword','110', '45', '27');
+CALL insert_equipment_weapon('War Hamer','70', '41', '9');
+
+
+
+
 
 
 SELECT * FROM see_players;
+SELECT * FROM see_equipment;
+SELECT * FROM see_weapons;
+
+
